4f59da4228c6dd41e3d5a2cfe569443ff2d85714
SYEDALIASDAQUE2210 <75180496+SYEDALIASDAQUE2210@users.noreply.github.com>
2023-05-21 00:55:51 +0530

    Update Day_1.py

--- Freq_Placement_QNS_Python/Day_1.py
+++ Freq_Placement_QNS_Python/Day_1.py
@@ -1,1 +1,6 @@
 Write a Python program to check if a string is a palindrome.
+ str=input(("Enter string :"))  
+if(str==str[::-1]):  
+      print("palindrome")  
+else:  
+      print("not a palindrome")  
